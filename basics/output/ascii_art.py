f158cec80f2b7cb811d63cd38f891ef8fc9336a0
repl.it user <replituser@example.com>
2020-10-22 13:10:27 +0000

    Added code to display an ASCII robot with dynamic eyes.

--- basics/output/ascii_art.py
+++ basics/output/ascii_art.py
@@ -1,7 +1,31 @@
 # Display Head
 print("  ##########")
-print("##          ##")
-print("##          ##")
+print("##  o    o  ##")
+print("##   ----   ##")
+print("  ##########")
+
+# Display body
+print("     ####")
+print("  ##      ##")
+print("  ##      ##")
+print("  ##      ##")
+print("     ####")
+
+
+# Asks user to enter their name
+print("What is your name?")
+name = input()
+print("Nice to meet you " + name + ".")
+
+# Changing eyes
+print("By pressing anykey on the keyboard, you can choose what my eyes look like")
+print("\"Give it ago")
+eyeCharacter = input()
+
+# Display Head
+print("  ##########")
+print("##  "+ eyeCharacter + "    " +eyeCharacter + "  ##")
+print("##   ----   ##")
 print("  ##########")
 
 # Display body
