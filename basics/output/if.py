7baa9c20c5856d8ee261eb9d1fb049c264a727c3
repl.it user <replituser@example.com>
2020-10-22 16:08:17 +0000

    Comments added to if

--- basics/output/if.py
+++ basics/output/if.py
@@ -1,7 +1,11 @@
+# Introduction
 print("What type of book is this?")
+# Setting the book
 book = str("adventure book")
 
+# Check the book state
 if(book == "adventure book"):
   print("I like adventure books!")
 
+# Print message
 print("Finished reading book.")
