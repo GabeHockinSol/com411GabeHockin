9726042272dde5a5b9dc59faaec0ba9f5b744fd8
repl.it user <replituser@example.com>
2020-10-22 15:59:18 +0000

    Added code to demonstrate the use of an if statement

---
+++ basics/output/if.py
@@ -0,0 +1,7 @@
+print("What type of book is this?")
+book = str("adventure book")
+
+if(book == "adventure book"):
+  print("I like adventure books!")
+
+print("Finished reading book.")
---
+++ basics/output/review.py
@@ -0,0 +1,25 @@
+# simple calculator
+
+# Introduction
+print("simple addition calculator\n")
+print("##########")
+print("#        #")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("\n")
+# Get the players input
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+result = num1 + num2
+
+# Show result
+print("##########")
+print("#"+"    "+str("{:.2f}".format(result))+        "#")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
+print("##########")
