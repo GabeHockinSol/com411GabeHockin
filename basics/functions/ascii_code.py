03c7e5a49ca6e5bba1d7e5382143b9a418fcad15
repl.it user <replituser@example.com>
2020-10-30 17:15:37 +0000

    Added code to demonstrate the use of built-in functions: len,ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,13 @@
+print("Program Started!")
+
+print("Please enter a standard character:")
+character = input()
+
+if (len(character) == 1):
+    print("The ASCII code for {} is {}".format(character, ord(character)))
+else:
+    print("A single character was expected.")
+
+print("Program Ended!")
+
+"Added code to demonstrate the use of built-in functions: len, ord."
