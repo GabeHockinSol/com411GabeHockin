768f04a691d784e31848c0d9f3218aa01fe0ca03
repl.it user <replituser@example.com>
2020-10-30 21:00:32 +0000

    Added code to demonstrate a simple lists.

---
+++ data/lists/simple_lists.py
@@ -0,0 +1,13 @@
+# function declaring directions
+def directions():
+  directions = ["Move Forward", "Move Backward", "Turn Left", "Turn Right"]
+  return directions
+
+# print directions to log
+def run():
+  print(directions())
+
+# call run
+run()
+
+"Added code to demonstrate a simple list."
