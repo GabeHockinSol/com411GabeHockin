3546d2f86bde20f47312a9983f2d6af186c02a01
repl.it user <replituser@example.com>
2020-10-30 21:25:26 +0000

    Added code to demonstrate a simple tuple.

---
+++ data/lists/simple_tuple.py
@@ -0,0 +1,10 @@
+def likelihood():
+  likelihoods = (50, 38, 27, 99, 4)
+  return min(likelihoods)
+
+def run():
+  print(f"Minimum likelihood of falling: {likelihood()}%")
+
+run()
+
+ "Added code to demonstrate a simple tuple."
