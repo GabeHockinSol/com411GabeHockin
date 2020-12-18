0883e11b045f0a917c1484acb46df32b1fa55e1a
repl.it user <replituser@example.com>
2020-12-18 16:40:19 +0000

    added basic human and robot classes

--- oop/robot.py
+++ oop/robot.py
@@ -2,7 +2,7 @@ class Robot:
 
   MAX_ENERGY = 100
   
-  def __init__(self, name = "Robot" , age = 0):
+  def __init__(self, name = "Robot", age = 0):
     self.name = name
     self.age = age
     self.energy = Robot.MAX_ENERGY
